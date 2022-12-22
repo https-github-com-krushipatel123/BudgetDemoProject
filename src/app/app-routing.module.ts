e7d3b9b30423d889dca5731b88615723da1e14df
Bhavesh Sasaniya <bhavesh.sasaniya@autofacets.in>
2022-12-22 12:11:08 +0530

    chnages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Budget } from './bdget-demo-project/budget.model';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'budget',
+    component: Budget
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BdgetDemoProjectComponent } from './bdget-demo-project/bdget-demo-project.component';
+import { LongpressDirective } from './longpress.directive';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxDocViewerModule } from 'ngx-doc-viewer';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BdgetDemoProjectComponent,
+    LongpressDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    NgxExtendedPdfViewerModule,
+    NgbModule,
+    NgxDocViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
